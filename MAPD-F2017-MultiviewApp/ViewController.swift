b638691bc7529e13c82e306e7db9ac6aa4a334c7
Sergio Brunacci <sergiodealmeidabrunacci@Sergios-MacBook-Pro.local>
2017-10-04 19:32:43 -0400

    Initial Commit

---
+++ MAPD-F2017-MultiviewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD-F2017-MultiviewApp
+//
+//  Created by Sergio de Almeida Brunacci on 2017-10-04.
+//  Copyright © 2017 Sergio de Almeida Brunacci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
